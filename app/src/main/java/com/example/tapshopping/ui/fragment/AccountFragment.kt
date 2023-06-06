f2bea5234f22a9fc38aab9ff0a02d2261b6ef611
whyhay01 <hyaaquub@gmail.com>
2023-06-06 12:06:16 +0100

    initializes now show

--- app/src/main/java/com/example/tapshopping/ui/fragment/AccountFragment.kt
+++ app/src/main/java/com/example/tapshopping/ui/fragment/AccountFragment.kt
@@ -1,36 +1,27 @@
 package com.example.tapshopping.ui.fragment
 
-import android.app.Activity
-import android.content.Intent
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.view.isVisible
 import android.widget.TextView
-import android.widget.Toast
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.tapshopping.R
 import com.example.tapshopping.data.local.DataStoreManager
 import com.example.tapshopping.databinding.FragmentAccountBinding
-import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.collect
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
-import java.io.IOException
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @AndroidEntryPoint
 class AccountFragment : Fragment(), View.OnClickListener {
 
     lateinit var binding: FragmentAccountBinding
-    lateinit var profileTextView: TextView
 
     @Inject
     lateinit var dataStoreManager: DataStoreManager
@@ -52,21 +43,6 @@ class AccountFragment : Fragment(), View.OnClickListener {
         binding.csAdmin.setOnClickListener(this)
         binding.csProfile.setOnClickListener(this)
 
-        val bottomNavigationView = requireActivity().findViewById<BottomNavigationView>(R.id.bottomNav)
-        bottomNavigationView.visibility = View.VISIBLE
-        profileTextView = binding.userProfileTextView
-        val firstName = DataStoreManager.FULL_NAME
-        val lastName = DataStoreManager.EMAIL
-        val shortName = firstName.first().toString() + lastName.first().toString()
-        profileTextView .text = shortName
-
-        binding.fullName.text= DataStoreManager.FULL_NAME
-        binding.email.text= DataStoreManager.EMAIL
-
-
-        binding.csAdmin.setOnClickListener (this)
-        binding.csProfile.setOnClickListener (this)
-
     }
 
     override fun onClick(v: View?) {
@@ -86,7 +62,8 @@ class AccountFragment : Fragment(), View.OnClickListener {
 
     private fun addProfileDetail() {
         binding.apply {
-            fullName.text = dataStoreManager.fullName
+            val expectedFullName = dataStoreManager.fullName
+            fullName.text = expectedFullName
             email.text = dataStoreManager.email
             twType.text = dataStoreManager.userType
             username.text = dataStoreManager.userName
@@ -96,7 +73,10 @@ class AccountFragment : Fragment(), View.OnClickListener {
                     csAdmin.isVisible = isAdmin
                 }
             }
-
+            val separatedNames: List<String> = expectedFullName.split(" ")
+            Log.d("AccountFragment ", "separatedNames: $separatedNames ")
+            val shortName:String = separatedNames[0].first() + separatedNames[1].first().toString()
+            userProfileTextView.text = shortName
         }
     }
 }
